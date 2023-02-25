2a52f5d3df4c9b6fa4d550961b1be4b4eb955c58
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-02-25 16:34:36 +0400

    error controller

--- learn-express/app.js
+++ learn-express/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const errorRoutes = require('./controllers/error');
 
 // set up a server with express
 const app = express();
@@ -27,9 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	// res.status(404).sendFile(path.join(__dirname, 'views', 'error404.html'));
-	res.status(404).render('error404');
-});
+app.use(errorRoutes.get404);
 
 app.listen(3000);
