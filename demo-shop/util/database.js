9d3e41cd54a4d4d275902c41c744c3495a68c9f6
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-03-02 00:39:25 +0400

    database connection

--- demo-shop/util/database.js
+++ demo-shop/util/database.js
@@ -4,3 +4,24 @@ const { mongoPassword, mongoLogin } = require('./credentials');
 
 let _db;
 
+const mongoConnect = (cb) => {
+	MongoClient.connect(`mongodb+srv://${mongoLogin}:${mongoPassword}@cluster0.jt3fsu1.mongodb.net/testdb?retryWrites=true&w=majority`)
+		.then(client => {
+			_db = client.db();
+			return cb();
+		})
+		.catch(err => {
+			console.log('DB connectopn err: ', err);
+			throw err;
+		});
+}
+
+const getDb = () => {
+	if (_db) {
+		return _db;
+	}
+	throw 'Can\'t connect to Mongodb';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
