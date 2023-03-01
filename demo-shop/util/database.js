87d0b9ea9d781245e1dc4545506e46058fd42835
Irina Shebarshina <76280175+rekilina@users.noreply.github.com>
2023-03-01 18:05:29 +0400

    Update database.js

--- demo-shop/util/database.js
+++ demo-shop/util/database.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('mongodb+srv://ivshebarshina:g5e5BFUHcxVyhnVs@cluster0.jt3fsu1.mongodb.net/?retryWrites=true&w=majority')
+MongoClient.connect('mongodb+srv://ivshebarshina:<pass>@cluster0.jt3fsu1.mongodb.net/?retryWrites=true&w=majority')
 	.then(result => {
 		console.log('DB connected');
 	})
