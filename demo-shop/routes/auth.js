85a4fb70c6de546db0a84fc265e7e1b2012c505e
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-03-10 19:19:26 +0400

    asynchronous validation

--- demo-shop/routes/auth.js
+++ demo-shop/routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const router = express.Router();
-
+const User = require('../models/user');
 const authController = require('../controllers/auth');
 
 router.get('/login', authController.getLogin);
@@ -13,7 +13,15 @@ router.post('/login', authController.postLogin);
 router.get('/signup', authController.getSignup);
 
 router.post('/signup',
-	check('email').isEmail().withMessage('invalid email'),
+	check('email').isEmail().custom((value, { req }) => {
+		return User.findOne({ email: value })
+			.then(userDoc => {
+				if (userDoc) {
+					// throw new Error('email is already registered');
+					return Promise.reject('email is already registered');
+				}
+			})
+	}),
 	check('password').isStrongPassword(),
 	check('confirm_password').custom((value, { req }) => {
 		if (value !== req.body.password) {
