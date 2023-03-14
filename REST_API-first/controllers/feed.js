b5fb12001a241e9f297f65285f3d128086966d12
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-03-14 23:47:39 +0400

    create (post) post route

--- REST_API-first/controllers/feed.js
+++ REST_API-first/controllers/feed.js
@@ -1,8 +1,24 @@
 exports.getPosts = (req, res, next) => {
-	res.json({
+	res.status(200).json({
 		posts: [{
 			title: "post title",
 			contect: "this is post"
 		}]
 	});
+}
+
+exports.createPost = (req, res, next) => {
+	// create it in the db
+
+	const title = req.body.title;
+	const content = req.body.content;
+
+	res.status(201).json({
+		message: "Post created successfully",
+		post: {
+			id: new Date().toString(),
+			title: title,
+			content: content
+		}
+	})
 }
