a41c5961241f8f3ad67fc8083b7beba8f0cfb7fe
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-03-15 16:25:02 +0400

    validation on server side

--- REST_API-first/controllers/feed.js
+++ REST_API-first/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
 		posts: [{
@@ -15,7 +17,13 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
 	// create it in the db
-
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			message: "Validation failed on createPost route.",
+			errors: errors
+		});
+	}
 	const title = req.body.title;
 	const content = req.body.content;
 
--- REST_API-first/routes/feed.js
+++ REST_API-first/routes/feed.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPost);
+router.post('/post', [
+	body('title').trim().isLength({ min: 5 }).notEmpty(),
+	body('content').trim().isLength({ min: 5 }).notEmpty()
+], feedController.createPost);
 
 module.exports = router;
