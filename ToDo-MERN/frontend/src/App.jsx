4f40d6ce17b2bb38ca428478eada26287809e70f
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-03-21 12:26:30 +0400

    init frontend

---
+++ ToDo-MERN/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='card'>App</div>
+  )
+}
+
+export default App;
