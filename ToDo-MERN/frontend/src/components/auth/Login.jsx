a841cfb08f59b7b403316730a661541d11a9979b
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-03-21 18:09:40 +0400

    login & register

--- ToDo-MERN/frontend/src/components/auth/Login.jsx
+++ ToDo-MERN/frontend/src/components/auth/Login.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
+import axios from 'axios';
 import classes from './AuthForm.module.scss';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  return (
-	<div className={classes.register}>
-		<h1 className={classes.title}>Login</h1>
-		<form className={classes.authForm}>
-			<label htmlFor="email">
-				Email
-				<input type="email" name="email" placeholder="email" required />
-			</label>
-			<label htmlFor="password">
-				Password
-				<input type="password" name="password" placeholder="password" required />
-			</label>
-		</form>
-	</div>
-  )
+	const navigate = useNavigate();
+
+	const login = async (e) => {
+		e.preventDefault();
+		const user = {
+			email: e.target.email.value,
+			password: e.target.password.value,
+		}
+		console.log(user);
+		try {
+			await axios.post('/api/auth/login', user);
+			toast.success('Login success');
+			navigate('/');
+		} catch (err) {
+			console.log('login function: ', err);
+			toast.error('Login failed');
+		}
+	}
+	return (
+		<div className={classes.register}>
+			<h1 className={classes.title}>Login</h1>
+			<form className={classes.authForm} onSubmit={login}>
+				<label htmlFor="email">
+					Email
+					<input type="email" name="email" placeholder="email" required />
+				</label>
+				<label htmlFor="password">
+					Password
+					<input type="password" name="password" placeholder="password" required />
+				</label>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	)
 }
 
 export default Login
--- ToDo-MERN/frontend/src/components/auth/Register.jsx
+++ ToDo-MERN/frontend/src/components/auth/Register.jsx
@@ -1,26 +1,46 @@
+import axios from 'axios';
 import React from 'react';
 import classes from './AuthForm.module.scss';
+import toast from 'react-hot-toast';
 
 function Register() {
-  return (
-	<div className={classes.register}>
-		<h1 className={classes.title}>Login</h1>
-		<form className={classes.authForm}>
-			<label htmlFor="name">
-				Name
-				<input type="text" name="name" placeholder="name" required />
-			</label>
-			<label htmlFor="email">
-				Email
-				<input type="email" name="email" placeholder="email" required />
-			</label>
-			<label htmlFor="password">
-				Password
-				<input type="password" name="password" placeholder="password" required />
-			</label>
-		</form>
-	</div>
-  )
+	const register = async (e) => {
+		e.preventDefault();
+		const user = {
+			name: e.target.name.value,
+			email: e.target.email.value,
+			password: e.target.password.value,
+		}
+		console.log(user);
+		try {
+			await axios.post('/api/auth/register', user);
+			toast.success('Register success');
+		} catch (err) {
+			console.log('register function: ', err);
+			toast.error('Register failed');
+		}
+	}
+
+	return (
+		<div className={classes.register}>
+			<h1 className={classes.title}>Register</h1>
+			<form className={classes.authForm} onSubmit={register} >
+				<label htmlFor="name">
+					Name
+					<input type="text" name="name" placeholder="name" required />
+				</label>
+				<label htmlFor="email">
+					Email
+					<input type="email" name="email" placeholder="email" required />
+				</label>
+				<label htmlFor="password">
+					Password
+					<input type="password" name="password" placeholder="password" required />
+				</label>
+				<button type="submit">Register</button>
+			</form>
+		</div>
+	)
 }
 
 export default Register;
