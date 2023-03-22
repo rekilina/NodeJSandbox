891b76445c9b38c093e113f28b8cf8b4814930ce
Irina Shebarshina <iv.shebarshina@gmail.com>
2023-03-22 22:01:26 +0400

    redirect auth if logged in

--- ToDo-MERN/frontend/src/pages/Auth.jsx
+++ ToDo-MERN/frontend/src/pages/Auth.jsx
@@ -1,16 +1,25 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Register from '../components/auth/Register'
 import Login from '../components/auth/Login'
 import Layout from '../components/Layout'
 import classes from './Auth.module.scss'
+import useAuth from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
 
 export default function Auth() {
+  const auth = useAuth();
+  const navigation = useNavigate();
+  useEffect(() => {
+    if (auth) {
+      navigation('/');
+    }
+  }, [auth, navigation]);
   return (
     <Layout>
       <div className={classes.form_container}>
-        <Login/>
-        <Register/>
+        <Login />
+        <Register />
       </div>
     </Layout>
   )
